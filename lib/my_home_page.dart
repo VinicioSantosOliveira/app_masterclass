b2d6ecb216c29a018b07580d6905df3d832c1212
Vinicio Oliveira <viniciodossantosoliveira@hotmail.com>
2024-02-13 19:50:45 -0300

    quase terminado container

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -108,20 +108,75 @@ class MyHomePage extends StatelessWidget {
                                   fontSize: 16,
                                 ),
                               ),
-                              const Padding(
-                                padding: EdgeInsets.only(left: 130),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 130),
                                 child: Text(
-                                  'Exercicíos: ',
+                                  'Exercicíos: 4',
                                   style: TextStyle(
-                                    color: Color.fromARGB(81, 86, 90, 1),
+                                    color: Colors.grey[700],
                                     fontSize: 12,
                                   ),
                                 ),
                               ),
                             ],
                           ),
-                          const Text('data'),
-                          const Row(),
+                          Padding(
+                            padding: const EdgeInsets.all(14.0),
+                            child: SizedBox(
+                              height: 40,
+                              width: size.width * 1,
+                              child: Text(
+                                'Estudos sobre animações implícitas e controladas, contendo4 exercícios e 2 estudos',
+                                style: TextStyle(color: Colors.grey[700]),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 5),
+                                  child: Image.asset(
+                                    'assets/images/Icon awesome-github.png',
+                                    height: 20,
+                                    width: 20,
+                                  ),
+                                ),
+                                const Text(
+                                  'Acessar código fonte',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 100),
+                                  child: SizedBox(
+                                    height: 30,
+                                    width: 95,
+                                    child: TextButton(
+                                      style: const ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStatePropertyAll(
+                                          Colors.blue,
+                                        ),
+                                      ),
+                                      onPressed: () {},
+                                      child: const Text(
+                                        'Ver mais',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
                     ),
